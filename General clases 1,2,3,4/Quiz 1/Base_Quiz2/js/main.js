d62ffa1a23ae91f51ed2405aa0d6b2a924f3ff7b
Dian7875 <dian7875.a@gmail.com>
2023-09-01 09:20:15 -0600

    ba

--- General clases 1,2,3,4/Quiz 1/Base_Quiz2/js/main.js
+++ General clases 1,2,3,4/Quiz 1/Base_Quiz2/js/main.js
@@ -1,4 +1,16 @@
 window.onload = (event) => {
     console.log("page is fully loaded");
+    let CardInput = document.getElementById("input");
+    CardInput.addEventListener("change",(event)=>{
+        console.log(event.target.value);
+    });
+
+    
+    let btnSave = document.getElementById("BTN");
+    btnSave.addEventListener("click",(event)=>{
+        console.log(CardInput.value);
+    })
+
+
 
 };
