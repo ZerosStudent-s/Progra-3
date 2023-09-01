4365af17f3f4c4490ae9cee60d72fd0e87f647e0
Dian7875 <dian7875.a@gmail.com>
2023-09-01 09:39:45 -0600

    zero

--- General clases 1,2,3,4/Quiz 1/Base_Quiz2/js/main.js
+++ General clases 1,2,3,4/Quiz 1/Base_Quiz2/js/main.js
@@ -4,13 +4,27 @@ window.onload = (event) => {
     CardInput.addEventListener("change",(event)=>{
         console.log(event.target.value);
     });
-
     
-    let btnSave = document.getElementById("BTN");
-    btnSave.addEventListener("click",(event)=>{
-        console.log(CardInput.value);
-    })
+    const cardText = document.getElementById("input");
+    const my_cards = document.getElementById("my_cards");
+    
 
 
+    let createCard = document.getElementById("BTN")
+    createCard.addEventListener("click", function() {
+        const text = cardText.value;
+        if (text.trim() !== "") {
+            // Crear una nueva tarjeta
+            const my_card = document.createElement("div");
+            my_card.className = "my_card";
+            my_card.textContent = text;
+            
+            // Agregar la tarjeta al contenedor
+            my_cards.appendChild(my_card);
+            
+            // Limpiar el campo de entrada de texto
+            cardText.value = "";
+        }
+    });
 
 };
