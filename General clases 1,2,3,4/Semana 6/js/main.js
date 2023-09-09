8747f8a1f3786fd3cbeb60ac4bb56638fd257e4b
Dian7875 <dian7875.a@gmail.com>
2023-09-09 14:15:43 -0600

    h

--- General clases 1,2,3,4/Semana 6/js/main.js
+++ General clases 1,2,3,4/Semana 6/js/main.js
@@ -1,6 +1,6 @@
 //!Jalar por variables los elementos del html
 
-
+let img = document.getElementById("img");
 window.addEventListener("load", function (event) {
     console.log("'Todos los recursos terminaron de cargar!");
     fetch('./js/mocks/company.json')
@@ -9,10 +9,17 @@ window.addEventListener("load", function (event) {
      
     console.log(company)
       //!Definir las variables con los datos del json
-    
+    img.src = company.img
     document.getElementById('companyName').innerText=company.companyName;
     document.getElementById('descripcion').innerText=company.description;//! documento elemento id del elemento inner text para 
     
+    console.log(company, company.services);
+      let DescC = document.getElementById("DesC")
+      company.services.forEach(service => {
+      const myserv = document.createElement(`desc_${service.id}`).innerText = service.description; 
+      myserv.appendChild(DescC);
+
+      });
     })
     
 
