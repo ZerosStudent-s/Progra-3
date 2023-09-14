02c15266f8e861be38058b1771153ab126fe86e6
Dian7875 <dian7875.a@gmail.com>
2023-09-14 02:45:56 -0600

    A

--- General clases 1,2,3,4/Semana 6/js/main.js
+++ General clases 1,2,3,4/Semana 6/js/main.js
@@ -1,26 +1,39 @@
 //!Jalar por variables los elementos del html
-
-let img = document.getElementById("img");
 window.addEventListener("load", function (event) {
     console.log("'Todos los recursos terminaron de cargar!");
     fetch('./js/mocks/company.json')
     .then(response => response.json())
     .then(company => {
-     
-    console.log(company)
-      //!Definir las variables con los datos del json
-    img.src = company.img
-    document.getElementById('companyName').innerText=company.companyName;
+    //!Definir las variables con los datos del json
+    const imageCompany = document.getElementById("CompanyImage");
+    imageCompany.src = company.img;
+    document.getElementById('companyName').innerText=company.companyName;//!Para textos fijos no es necesario traer el elemento padre de donde se añadiran
     document.getElementById('descripcion').innerText=company.description;//! documento elemento id del elemento inner text para 
+
+
+    company.services.forEach(services => {
+      const contenedor = this.document.getElementById("Services");
+      const ico = this.document.createElement("img");
+      const descripcion = document.createElement("p");
+      ico.src = services.ico;
+      ico.alt = services.id;
+      descripcion.textContent = services.description;
     
-    console.log(company, company.services);
-      let DescC = document.getElementById("DesC")
-      company.services.forEach(service => {
-      const myserv = document.createElement(`desc_${service.id}`).innerText = service.description; 
-      myserv.appendChild(DescC);
+      const itemContenedor = document.createElement("div");
+      itemContenedor.className = "ServClas";
+      ico.className = "ServICon";
+      itemContenedor.appendChild(ico);
+      itemContenedor.appendChild(descripcion);
+
+      contenedor.appendChild(itemContenedor);
+    
+
+    });
 
       });
+
+      ;
     })
     
 
-  });
+
